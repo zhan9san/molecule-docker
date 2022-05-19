66948dee98fd35bef810eedcf4dd3aebb2eb6fcb
Jack Zhang <jack4zhang@gmail.com>
2022-05-19 10:47:17 +0800

    test role with docker driver

--- src/molecule_docker/test/test_func.py
+++ src/molecule_docker/test/test_func.py
@@ -24,34 +24,35 @@ def test_command_init_and_test_scenario(tmp_path: pathlib.Path, DRIVER: str) -> 
     shutil.rmtree(tmp_path, ignore_errors=True)
     tmp_path.mkdir(exist_ok=True)
 
+    namespace = "foo"
+    role_name = "bar"
     scenario_name = "default"
 
     with change_dir_to(tmp_path):
 
-        scenario_directory = tmp_path / "molecule" / scenario_name
+        scenario_directory = tmp_path / role_name / "molecule" / scenario_name
         cmd = [
             "molecule",
             "init",
-            "scenario",
+            "role",
             "--driver-name",
             DRIVER,
+            f"{namespace}.{role_name}",
         ]
         result = run_command(cmd)
         assert result.returncode == 0
 
         assert scenario_directory.exists()
 
-        # run molecule reset as this may clean some leftovers from other
-        # test runs and also ensure that reset works.
-        result = run_command(["molecule", "reset"])  # default sceanario
-        assert result.returncode == 0
+        with change_dir_to(role_name):
+            # run molecule reset as this may clean some leftovers from other
+            # test runs and also ensure that reset works.
+            result = run_command(["molecule", "reset"])  # default sceanario
+            assert result.returncode == 0
 
-        result = run_command(["molecule", "reset", "-s", scenario_name])
-        assert result.returncode == 0
-
-        cmd = ["molecule", "--debug", "test", "-s", scenario_name]
-        result = run_command(cmd)
-        assert result.returncode == 0
+            cmd = ["molecule", "--debug", "test"]
+            result = run_command(cmd)
+            assert result.returncode == 0
 
 
 def test_command_static_scenario() -> None:
